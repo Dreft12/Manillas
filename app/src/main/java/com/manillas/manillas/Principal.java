757619a1b19d043b8e253a5b3563cdf3cc03e28c
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-08-23 20:48:22 -0500

    V 0.3 Interfaz
    Agregando interfaz, agregando archivo Strings inicial

--- app/src/main/java/com/manillas/manillas/Principal.java
+++ app/src/main/java/com/manillas/manillas/Principal.java
@@ -1,13 +1,21 @@
 package com.manillas.manillas;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class Principal extends AppCompatActivity {
-
+    private Spinner cmbMater, cmbDije, cmbTipo, cmbMoneda;
+    private Resources resources;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        cmbMater = (Spinner) findViewById(R.id.cmbMater);
+        cmbDije = (Spinner) findViewById(R.id.cmbDije);
+        cmbTipo = (Spinner) findViewById(R.id.cmbTipo);
+        cmbMoneda = (Spinner) findViewById(R.id.cmbMoneda);
+        resources = this.getResources();
     }
 }
