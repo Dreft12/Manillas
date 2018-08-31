d22821578acabb1a37bb21cfe52c8036183bf001
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-08-30 20:00:38 -0500

    V 2.3 Agregando Test
    Se agregó los tests para cada case del switch de metodos para comprobar su funcionamiento.

--- app/src/test/java/com/manillas/manillas/ExampleUnitTest.java
+++ app/src/test/java/com/manillas/manillas/ExampleUnitTest.java
@@ -11,7 +11,88 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void cuero_martillo_oro(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 1, cmbTipo = 1;
+        assertEquals(100,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuero_martillo_oroRosado(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 1, cmbTipo = 2;
+        assertEquals(100,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuero_martillo_Plata(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 1, cmbTipo = 3;
+        assertEquals(80,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuero_martillo_Niquel(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 1, cmbTipo = 4;
+        assertEquals(70,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuero_ancla_oro(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 2, cmbTipo = 1;
+        assertEquals(120,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuero_ancla_oroRosado(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 2, cmbTipo = 2;
+        assertEquals(120,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuero_ancla_Plata(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 2, cmbTipo = 3;
+        assertEquals(100,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuero_ancla_Niquel(){
+        int cant = 1, cmbMaterial = 1, cmbDije = 2, cmbTipo = 4;
+        assertEquals(90,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_martillo_oro(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 1, cmbTipo = 1;
+        assertEquals(90,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_martillo_oroRosado(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 1, cmbTipo = 2;
+        assertEquals(90,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_martillo_Plata(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 1, cmbTipo = 3;
+        assertEquals(70,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_martillo_Niquel(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 1, cmbTipo = 4;
+        assertEquals(50,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_ancla_oro(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 2, cmbTipo = 1;
+        assertEquals(110,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_ancla_oroRosado(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 2, cmbTipo = 2;
+        assertEquals(110,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_ancla_Plata(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 2, cmbTipo = 3;
+        assertEquals(90,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void cuerda_ancla_Niquel(){
+        int cant = 1, cmbMaterial = 2, cmbDije = 2, cmbTipo = 4;
+        assertEquals(80,Metodos.total(cmbMaterial,cmbDije,cmbTipo,cant));
+    }
+    @Test
+    public void dolarAPesos(){
+        int dolar = 1;
+        assertEquals(3200,Metodos.dolarAPeso(dolar));
     }
 }
